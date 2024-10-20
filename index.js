9f13538a1c16a8865060413a7f1e9de27e14a0eb
harshbhalala-04 <harsh.bhalala@pirimidtech.com>
2024-10-20 11:42:19 +0530

    feat: add axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
